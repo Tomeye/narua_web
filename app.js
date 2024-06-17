2ab8b7fb6bbd26d752c302b521d87ad3d59cb99a
Tomeye <meyer.tobias96@gmail.com>
2024-06-17 21:38:13 +0200

    try splash with image

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-window.onload = () => {
-    console.log('on load');
-};
+const splash = document.querySelector('#splashContainer');
+
+window.addEventListener("load", (event) => {
+    console.log('content start loading');
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    console.log('content loaded');
+    setTimeout(() => {
+        splash.classList.add('display-none');
+    }, 2000);
+});
