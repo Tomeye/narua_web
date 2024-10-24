44d6e281ba420dcab67f787a8dd8e937e40645be
Tobias Meyer <meyer.tobias96@gmail.com>
2024-10-24 21:31:13 +0200

    rename public to docs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('../narua_web'));
+app.use(express.static('/public'));
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
