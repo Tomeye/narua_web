e3ca981f9ef1024cd92cda368364fc8ada458444
Tobias Meyer <meyer.tobias96@gmail.com>
2024-10-24 21:11:33 +0200

    use express static file server

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.static('../narua_web'));
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
--- splash.js
+++ splash.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-});
-
-
 const splash = document.querySelector("#splash-container");
 
 window.addEventListener("load", (event) => {
